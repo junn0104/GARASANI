544d9945d7e0fb99af00fd9bbb719bf86fd0b0ff
ishs311213 <65996056+junn0104@users.noreply.github.com>
2024-10-29 16:40:58 +0900

    Update DataManage.py

--- DataManage.py
+++ DataManage.py
@@ -3,14 +3,20 @@ import json
 
 data_path = './data'
 
-init_data = {"reported_count": 0, "received_reports_count": 0, "tested_count": 0, "received_tests_count": 0, "ai_detected_count": 0}
-
+init_data = {
+    "phone_number": "",
+    "reported_count": 0,
+    "ai_detected_count": 0,
+    "recent10_percent": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+}
 
 def initialize(number):
     try:
         open(f'{data_path}/{number}.json', 'r')
     except:
         with open(f'{data_path}/{number}.json', 'w') as f:
+            x=init_data
+            init_data['phone_number']=number
             json.dump(init_data, f, indent=4)
 
 def read(number):
@@ -24,3 +30,42 @@ def update(number, data_name, data_value):
     data[data_name] = data_value
     with open(f'{data_path}/{number}.json', 'w') as f:
         json.dump(data, f, indent=4)
+
+def add_recent10(number, data_value):
+    with open(f'{data_path}/{number}.json', 'r') as f:
+        data = json.load(f)
+    data["recent10_percent"].append(data_value)
+    data["recent10_percent"].pop(0)
+    with open(f'{data_path}/{number}.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+def reported1(reporter, reported):
+    with open(f'{data_path}/{reported}.json', 'r') as f:
+        data = json.load(f)
+    data["reported_count"]+=1
+    with open(f'{data_path}/{reported}.json', 'w') as f:
+        json.dump(data, f, indent=4)
+    with open(f'{data_path}/reported.json', 'r') as f:
+        data = json.load(f)
+    new_report = {
+        "reporter": reporter,
+        "reported": reported
+    }
+    data.append(new_report)
+    with open(f'{data_path}/reported.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+def detected1(number):
+    with open(f'{data_path}/{number}.json', 'r') as f:
+        data = json.load(f)
+    data["ai_detected_count"]+=1
+    with open(f'{data_path}/{number}.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+def isreported(reporter, reported):
+    with open(f'{data_path}/reported.json', 'r') as f:
+        data = json.load(f)
+    for report in data:
+        if report["reporter"] == reporter and report["reported"] == reported:
+            return True
+    return False
